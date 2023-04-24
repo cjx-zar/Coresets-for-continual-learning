2124ff1e7deacf3ceba5b779375602ab19c85fda
cjx-zar <jxchenczar@foxmail.com>
2023-04-24 21:48:36 +0800

    old framework

--- scripts/train.py
+++ scripts/train.py
@@ -31,7 +31,8 @@ class MultiCEFocalLoss(torch.nn.Module):
         ids = target.view(-1, 1) 
         alpha = self.alpha[ids.data.view(-1)] # 注意，这里的alpha是给定的一个list(tensor),里面的元素分别是每一个类的权重因子
         probs = (pt * class_mask).sum(1).view(-1, 1) # 利用onehot作为mask，提取对应的pt
-        probs[probs < 0.2] = 1
+        # probs[probs < 0.3] = 1 - probs[probs < 0.3]
+        probs[probs < 0.2] = 0.8
         log_p = probs.log()
         # 同样，原始ce上增加一个动态权重衰减因子
         loss = -alpha * (torch.pow((1 - probs), self.gamma)) * log_p
